17ec3912fa20740f271e4bb3e5bd1dae49ece5cc
sora.nakao <asakaragyouza@gmail.com>
2024-04-07 05:23:17 +0900

    update

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -8,11 +8,12 @@ import (
 
 func reflectReply(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Test!")
+	fmt.Println(w)
 }
 
 func handleRequests() {
 	http.HandleFunc("/webhook", reflectReply)
-	log.Fatal(http.ListenAndServeTLS(":443", "letsencrypt.crt", "letsencrypt.key", nil))
+	log.Fatal(http.ListenAndServeTLS(":443", "/ssl/letsencrypt.crt", "/ssl/letsencrypt.key", nil))
 }
 
 func main() {
